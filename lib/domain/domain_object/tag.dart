6a59226971dd3991e079597f2695b27173388fb4
ko5 <kotyana1331@gmail.com>
2021-01-27 20:18:01 +0900

    feat: convert json and Tag

--- lib/domain/domain_object/tag.dart
+++ lib/domain/domain_object/tag.dart
@@ -8,4 +8,15 @@ class Tag {
     this.name,
     this.colorCode,
   });
+
+  Tag.fromJson(Map<String, dynamic> json)
+      : this.id = json["id"],
+        this.name = json["name"],
+        this.colorCode = json["colorCode"];
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "colorCode": colorCode,
+      };
 }
