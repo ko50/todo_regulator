fe0b88b8324872fa97410aaf56f08aa51b909fbc
ko5 <kotyana1331@gmail.com>
2021-01-31 00:13:15 +0900

    add: interface of TaskService

---
+++ lib/domain/service/interface/task_service.dart
@@ -0,0 +1,13 @@
+import 'package:todo_regulator/domain/model/task/task.dart';
+
+abstract class ITaskService {
+  Future<Task> create({String title, String description});
+
+  Future<Task> editTask({Task task, String title, String description});
+
+  Future<Task> appendTag({Task task, String tagId});
+
+  Future<Task> removeTag({Task task, String tagId});
+
+  Future<Task> deleteTask({Task task});
+}
