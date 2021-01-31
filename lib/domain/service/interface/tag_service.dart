ae08a0edb31fc8a3ad4e558c0afbf46a41f7d60a
ko5 <kotyana1331@gmail.com>
2021-01-31 15:22:07 +0900

    feat: Constructor of services

--- lib/domain/service/interface/tag_service.dart
+++ lib/domain/service/interface/tag_service.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/widgets.dart';
-import 'package:todo_regulator/domain/model/tag/tag.dart';
 
-abstract class TagService {
+import 'package:todo_regulator/domain/model/tag/tag.dart';
+import 'package:todo_regulator/domain/model/tag/tag_repository.dart';
+
+abstract class ITagService {
+  final TagRepository repository;
+
+  ITagService(this.repository);
+
   Future<Tag> create({@required String name, Color color});
 
   Future<Tag> editTask(Tag tag, String name);
--- lib/domain/service/interface/task_service.dart
+++ lib/domain/service/interface/task_service.dart
@@ -1,8 +1,13 @@
 import 'package:meta/meta.dart';
 
 import 'package:todo_regulator/domain/model/task/task.dart';
+import 'package:todo_regulator/domain/model/task/task_repository.dart';
 
 abstract class ITaskService {
+  final TaskRepository repository;
+
+  ITaskService(this.repository);
+
   Future<Task> create({@required String title, String description});
 
   Future<Task> editTask(Task task, {String title, String description});
