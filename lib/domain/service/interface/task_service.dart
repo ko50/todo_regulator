f65d107d753a9555349382082a26dba4a0e7a6e2
ko5 <kotyana1331@gmail.com>
2021-01-31 15:05:00 +0900

    modify: chaged method of catch TaskService argument

--- lib/domain/service/interface/task_service.dart
+++ lib/domain/service/interface/task_service.dart
@@ -1,13 +1,15 @@
+import 'package:meta/meta.dart';
+
 import 'package:todo_regulator/domain/model/task/task.dart';
 
 abstract class ITaskService {
-  Future<Task> create({String title, String description});
+  Future<Task> create({@required String title, String description});
 
-  Future<Task> editTask({Task task, String title, String description});
+  Future<Task> editTask(Task task, {String title, String description});
 
-  Future<Task> appendTag({Task task, String tagId});
+  Future<Task> appendTag(Task task, String tagId);
 
-  Future<Task> removeTag({Task task, String tagId});
+  Future<Task> removeTag(Task task, String tagId);
 
-  Future<Task> deleteTask({Task task});
+  Future<Task> deleteTask(Task task);
 }
